fd3d8dbcda792ebbc3bcaf01384cbffb99429b8a
KyleHsu-Advantech <kyle.hsu@advantech.com>
2026-08-26 17:19:47 +0800

    Update create_and_deploy.py

--- create_and_deploy.py
+++ create_and_deploy.py
@@ -20,4 +20,4 @@ import requests
 parser = argparse.ArgumentParser(description="Denodo Solution Manager CI deployment")
 parser.add_argument("--name", required=True, help="Revision name")
 args = parser.parse_args()
-return args.name
+print(args.name)
